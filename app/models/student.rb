05dc78f2cef6eb5c6f8737b7cd7052972c9a7f64
Maha <maheenbatool48@gmail.com>
2018-05-11 11:59:30 -0700

    Student model updated

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+  validates :roll_number, presence: true
+  validates :batch_number, presence: true
+  validates :semseter, presence: true
+  validates :department, presence: true
 end
